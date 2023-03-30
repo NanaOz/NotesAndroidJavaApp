ef3d224bfb7ddbca0952ff92a181ad70c935636e
Alina <alinavik93@gmail.com>
2023-03-31 00:27:52 +0500

    работает кнопка добавления и сохранения

--- app/src/main/java/com/example/notesandroidjavaapp/NotesTakerActivity.java
+++ app/src/main/java/com/example/notesandroidjavaapp/NotesTakerActivity.java
@@ -2,14 +2,25 @@ package com.example.notesandroidjavaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.notesandroidjavaapp.Models.Notes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NotesTakerActivity extends AppCompatActivity {
 
     EditText edit_text_title, edit_text_notes;
     ImageView image_view_save;
+
+    Notes notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +30,29 @@ public class NotesTakerActivity extends AppCompatActivity {
         image_view_save = findViewById(R.id.image_view_save);
         edit_text_title = findViewById(R.id.edit_text_title);
         edit_text_notes = findViewById(R.id.edit_text_notes);
+
+        image_view_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = edit_text_title.getText().toString();
+                String description = edit_text_notes.getText().toString();
+
+                if(description.isEmpty()) {
+                    Toast.makeText(NotesTakerActivity.this, "**добавьте описание**", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM yyyy HH:mm");
+                Date date = new Date();
+                notes = new Notes();
+                notes.setTitle(title);
+                notes.setNotes(description);
+                notes.setDate(formatter.format(date));
+
+                Intent intent = new Intent();
+                intent.putExtra("notes", notes);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
